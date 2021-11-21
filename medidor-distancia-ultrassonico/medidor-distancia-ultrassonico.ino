991ed1475873021f97e052d2eeaaff27ebee33b5
r0100 <lvinicius15@gmail.com>
2021-11-21 15:49:08 -0300

    Refatoração do código da trena ultrassônica
    O código da trena foi reestruturado para melhor organização e para adicionar novas funcionalidades

--- medidor-distancia-ultrassonico/medidor-distancia-ultrassonico.ino
+++ medidor-distancia-ultrassonico/medidor-distancia-ultrassonico.ino
@@ -4,8 +4,13 @@
 
 #define PINO_TRIGGER 3
 #define PINO_ECHO 2
-#define PINO_BT_UN 4
+
+#define BT_CONVERSAO 4
+#define BT_AREA 5
+#define BT_PITAGORAS 6
+
 #define TAXA_ATUALIZACAO 1000
+#define DISTANCIA_MAX 400 //4m
 
 enum Unidades {
   CMT,
@@ -18,44 +23,106 @@ Ultrasonic sensor(PINO_TRIGGER, PINO_ECHO);
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 Unidades unidade = CMT;
 
+float medirDistancia();
+void calcularArea();
+void calcularPitagoras();
+
+float converter(float distancia); //Converte de cm para qualquer outra unidade
+
+void imprimir(String linha1, String linha2);
+void imprimirDistancia(float distancia);
+
 void setup()
 {
-  pinMode(PINO_BT_UN, INPUT);
+  pinMode(BT_CONVERSAO, INPUT_PULLUP);
+  pinMode(BT_AREA, INPUT_PULLUP);
+  pinMode(BT_PITAGORAS, INPUT_PULLUP);
   lcd.init();
   lcd.backlight();
 }
 
 void loop()
 {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print(F("Distancia:"));
-  lcd.setCursor(0, 1);
+  float distancia = medirDistancia();
+  imprimirDistancia(distancia);
   
-  float distancia = sensor.read();
-  String texto = "";
-  switch (unidade) {
-    case POL:
-      texto+=String(distancia /= 2.51) + " pol";
-      break;
-    case MT:
-      texto+=String(distancia /= 100.00) + " m";
-      break;
-    case PES:
-      texto+=String(distancia /= 30,12) + " pes";
-      break;
-    default:
-      texto+=String(distancia) + " cm";
-  }
-  
-  lcd.print(texto);
-
   long tempo_ref = millis();
   while (millis() - tempo_ref < TAXA_ATUALIZACAO) {
-    if (digitalRead(PINO_BT_UN)) {
+    if (!digitalRead(BT_CONVERSAO)) {
       unidade = unidade < PES ? unidade + 1 : CMT;
-      while (digitalRead(PINO_BT_UN));
+      while (!digitalRead(BT_CONVERSAO));
       break;
     }
+    
+    if(!digitalRead(BT_AREA)) {
+      calcularArea();
+      break;
+    }
+    
+    if(!digitalRead(BT_PITAGORAS)) {
+      calcularPitagoras();
+      break;
+    }
+  }
+}
+
+float medirDistancia() {
+  return sensor.read();
+}
+
+void calcularArea() {
+  
+}
+
+void calcularPitagoras() {
+  
+}
+
+float converter(float distancia) {
+  switch(unidade) {
+    case CMT:
+    return distancia;
+    case POL:
+    return distancia/2.51;
+    case MT:
+    return distancia/100.0;
+    case PES:
+    return distancia/30.12;
+  }
+}
+
+void imprimir(String linha1, String linha2) {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(linha1);
+  lcd.setCursor(0, 1);
+  lcd.print(linha2);
+}
+
+void imprimirDistancia(float distancia) {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(F("Distancia"));
+  
+  lcd.setCursor(0, 1);
+
+  if(distancia > DISTANCIA_MAX) {
+    lcd.print(F("fora de escala"));
+    return;
+  }
+  
+  distancia = converter(distancia);
+  lcd.print(String(distancia));
+  lcd.print(F(" "));
+
+  switch(unidade) {
+    case CMT:
+    lcd.print(F("cm"));
+    case POL:
+    lcd.print(F("pol"));
+    case MT:
+    lcd.print(F("m"));
+    case PES:
+    lcd.print(F("pes"));
   }
 }
